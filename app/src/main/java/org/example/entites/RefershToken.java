812d39e9c280b41e186902eff47c9b1227aad904
Rishon <rishon.kumar.rahi@gmail.com>
2024-07-28 20:25:22 +0530

    Adding customer user detail

--- app/src/main/java/org/example/entites/RefershToken.java
+++ app/src/main/java/org/example/entites/RefershToken.java
@@ -1,2 +1,34 @@
-package org.example.entites;public class RefershToken {
+package org.example.entites;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Table(name = "tokens")
+public class RefershToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String token;
+
+    private Instant expireDate;
+
+    @OneToOne
+    @JoinColumn(name = "id",referencedColumnName = "user_id")
+    private UserInfo userInfo;
+
 }
--- app/src/main/java/org/example/entites/UserInfo.java
+++ app/src/main/java/org/example/entites/UserInfo.java
@@ -1,2 +1,35 @@
-package org.example.entites;public class UserInfo {
+package org.example.entites;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+public class UserInfo {
+
+    @Id
+    @Column(name="user_id")
+    private String userId;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns =@JoinColumn(name="role_id")
+    )
+    private Set<UserRole> roles = new HashSet<>();
+
 }
